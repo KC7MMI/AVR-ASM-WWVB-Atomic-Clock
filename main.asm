a7335ce30cf59afd6861132908e07ee9612cfd9f
Benjamin Russell <40191081+KC7MMI@users.noreply.github.com>
2019-05-29 17:00:03 -0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -53,9 +53,9 @@
 
 start:		
 ; Timer Setup
-	ldi tmp, 0			; Load 5 in r16
-	sts TCCR1A, tmp		; TC1 Control Reg A set to normal for simple counting operations
-	sts TCCR1C, tmp		; TC1 Control Reg C set to normal for simple counting operations
+	ldi tmp, 0			; Load 0 in r16
+	sts TCCR1A, tmp			; TC1 Control Reg A set to normal for simple counting operations
+	sts TCCR1C, tmp			; TC1 Control Reg C set to normal for simple counting operations
 
 ; SPI Setup
 	ldi tmp, 1<<SPI2X		; Set SPI clock to fosc/2 (8MHz)
@@ -114,47 +114,47 @@ start:
 ; Here we wait for the start of a pulse
 loop:	
 	sbic PINB, PINB7	; Keep looping until PINB7 is pressed, at which point skip rjmp loop
-	rjmp loop			; Loop until PINB7 pressed
+	rjmp loop		; Loop until PINB7 pressed
 	ldi tmp, 5
 	sts TCCR1B, r16		; Start TC1 and set prescaler to 1/1024
 
 ; Increment Seconds
 	ldi tmp, $59		; Prepare to load 59 into db0 IF:
-	sbrc mnr, 6			; If 6th marker received
+	sbrc mnr, 6		; If 6th marker received
 	mov db0, tmp		; Put 59 into register to trick into rolling over
-	inc db0				; Increment 1s
+	inc db0			; Increment 1s
 	ldi tmp, $0f		; Setup bitmask
 	and tmp, db0		; Copy only lower nibble into tmp
 	cpi tmp, $0a		; Compare
 	brne Print_Time		; If not time to rollover, go print time
 	ldi tmp, $f0		; Prepare for bitmask
 	and db0, tmp		; Clear lower nibble in register
-	swap db0			; Swap nibbles for easy increment
-	inc db0				; Increment 10s
-	swap db0			; Put 10s back in their place
+	swap db0		; Swap nibbles for easy increment
+	inc db0			; Increment 10s
+	swap db0		; Put 10s back in their place
 	mov tmp, db0		; Copy to tmp for compare
 	cpi tmp, $60		; Compare
 	brne Print_Time		; If not time to rollover, go print time
-	clr db0				; Reset register to 00
+	clr db0			; Reset register to 00
 
 ; Increment Minutes
-	inc db1				; Increment 1s
+	inc db1			; Increment 1s
 	ldi tmp, $0f		; Setup bitmask
 	and tmp, db1		; Copy only lower nibble into tmp
 	cpi tmp, $0a		; Compare
 	brne Print_Time		; If not time to rollover, go print time
 	ldi tmp, $f0		; Prepare for bitmask
 	and db1, tmp		; Clear lower nibble in register
-	swap db1			; Swap nibbles for easy increment
-	inc db1				; Increment 10s
-	swap db1			; Put 10s back in their place
+	swap db1		; Swap nibbles for easy increment
+	inc db1			; Increment 10s
+	swap db1		; Put 10s back in their place
 	mov tmp, db1		; Copy to tmp for compare
 	cpi tmp, $60		; Compare
 	brne Print_Time		; If not time to rollover, go print time
-	clr db1				; Reset register to 00
+	clr db1			; Reset register to 00
 
 ; Increment Hours
-	inc db2				; Increment 1s
+	inc db2			; Increment 1s
 	mov tmp, db2		; Copy only lower nibble into tmp
 	cpi tmp, $24		; Compare
 	breq Hr_Rollover	; Branch to rollover hour and continue
@@ -163,12 +163,12 @@ loop:
 	brne Print_Time		; If not time to rollover, go print time
 	ldi tmp, $f0		; Prepare for bitmask
 	and db2, tmp		; Clear lower nibble in register
-	swap db2			; Swap nibbles for easy increment
-	inc db2				; Increment 10s
-	swap db2			; Put 10s back in their place
+	swap db2		; Swap nibbles for easy increment
+	inc db2			; Increment 10s
+	swap db2		; Put 10s back in their place
 	rjmp Print_Time		; Print time after rolling over 1s
 Hr_Rollover:
-	clr db2				; Reset register to 00
+	clr db2			; Reset register to 00
 
 ; Increment Days
 
@@ -195,7 +195,7 @@ Print_Time:
 	ldi d2l, $00		; Display 2, NOP
 	rcall Display_Write	; Write changes to display via SPI
 	ldi d1h, $04		; Display 1, digit 4
-	swap d1l			; Display 1, seconds 10s
+	swap d1l		; Display 1, seconds 10s
 	rcall Display_Write	; Write changes to display via SPI
 
 ; Display Minutes
@@ -205,7 +205,7 @@ Print_Time:
 	ldi d2l, $00		; Display 2, NOP
 	rcall Display_Write	; Write changes to display via SPI
 	ldi d1h, $06		; Display 1, digit 6
-	swap d1l			; Display 1, minutes 10s
+	swap d1l		; Display 1, minutes 10s
 	rcall Display_Write	; Write changes to display via SPI
 
 ; Display Hours
@@ -215,7 +215,7 @@ Print_Time:
 	ldi d2l, $00		; Display 2, NOP
 	rcall Display_Write	; Write changes to display via SPI
 	ldi d1h, $08		; Display 1, digit 8
-	swap d1l			; Display 1, hours 10s
+	swap d1l		; Display 1, hours 10s
 	rcall Display_Write	; Write changes to display via SPI
 
 ; Display Days (100s & 10s)
@@ -225,13 +225,13 @@ Print_Time:
 	mov d2l, db3		; Display 2, days 10s
 	rcall Display_Write	; Write changes to display via SPI
 	ldi d2h, $03		; Display 2, digit 3
-	swap d2l			; Display 2, days 100s
+	swap d2l		; Display 2, days 100s
 	rcall Display_Write	; Write changes to display via SPI
 
 ; Display DUT1 Sign
 	ldi d1h, $02		; Display 1, digit 2
 	ldi d1l, $80		; Display 1, decimal point only
-	sbrc db4, 1			; Check for minus bit
+	sbrc db4, 1		; Check for minus bit
 	ldi d1l, $81		; Display 1, decimal point & minus sign (segment G)
 	ldi d2h, $00		; Display 2, NOP
 	ldi d2l, $00		; Display 2, NOP
@@ -242,7 +242,7 @@ Print_Time:
 	ldi d1l, $00		; Display 1, NOP
 	ldi d2h, $01		; Display 2, digit 1
 	mov d2l, db4		; Display 2, prepare to display days 1s
-	swap d2l			; Display 2, days 1s
+	swap d2l		; Display 2, days 1s
 	rcall Display_Write	; Write changes to display via SPI
 
 ; Display Year (10s)
@@ -262,11 +262,11 @@ Print_Time:
 ; Display LY/LS/DST
 	ldi d1h, $08		; Display 1, digit 8
 	ldi d1l, $00		; Display 1, clear
-	sbrc db6, 0			; Check for DST
+	sbrc db6, 0		; Check for DST
 	sbr d1l, 1<<6		; If not clear, turn on segment A for DST
-	sbrc db6, 2			; Check for LS
+	sbrc db6, 2		; Check for LS
 	sbr d1l, 1<<3		; If not clear, turn on segment D for LS
-	sbrc db6, 3			; Check for LY
+	sbrc db6, 3		; Check for LY
 	sbr d1l, 1<<0		; If not clear, turn on segment G for LY
 	ldi d2h, $00		; Display 2, NOP
 	ldi d2l, $00		; Display 2, NOP
@@ -277,7 +277,7 @@ Print_Time:
 	ldi d1l, $00		; Display 1, NOP
 	ldi d2h, $04		; Display 2, digit 4
 	mov d2l, db6		; Display 2, prepare to display years 1s
-	swap d2l			; Display 2, years 1s
+	swap d2l		; Display 2, years 1s
 	rcall Display_Write	; Write changes to display via SPI
 
 ; Checking for pulse lengths similar to those listed below and outputs to bit value register
@@ -287,7 +287,7 @@ Print_Time:
 Clock_Pulse:	
 	sbis PINB, PINB7	; Keep looping until PINB7 is released, at which point exit loop
 	rjmp Clock_Pulse	; Loop utnil key released
-	ldi tmp, 0			; For clearing the timer registers later
+	ldi tmp, 0		; For clearing the timer registers later
 	sts TCCR1B, tmp		; Stop TC1 to read values
 	lds XL, TCNT1L		; Load TC1 Low value into working register
 	lds XH, TCNT1H		; Load TC1 High value into working register
@@ -306,20 +306,20 @@ Set_Mrkr_Bit:
 	sbrc bvr, MRK		; Skip Set_Frame_Bit if no previous MRK
 	rjmp Set_Frame_Bit	; If previous pulse, Set_Frame_Bit
 	ldi bvr, 1<<MRK		; If no previous MRK, set MRK in bvr
-	lsl mnr				; Shift bit left for each marker
-	sbrc mnr, 1			; Skip if not #1 marker
+	lsl mnr			; Shift bit left for each marker
+	sbrc mnr, 1		; Skip if not #1 marker
 	mov db1, dbx		; Move complete byte into register
-	sbrc mnr, 2			; Skip if not #2 marker
+	sbrc mnr, 2		; Skip if not #2 marker
 	mov db2, dbx		; Move complete byte into register
-	sbrc mnr, 3			; Skip if not #3 marker
+	sbrc mnr, 3		; Skip if not #3 marker
 	mov db3, dbx		; Move complete byte into register
-	sbrc mnr, 4			; Skip if not #4 marker
+	sbrc mnr, 4		; Skip if not #4 marker
 	mov db4, dbx		; Move complete byte into register
-	sbrc mnr, 5			; Skip if not #5 marker
+	sbrc mnr, 5		; Skip if not #5 marker
 	mov db5, dbx		; Move complete byte into register
-	sbrc mnr, 6			; Skip if not #6 marker
+	sbrc mnr, 6		; Skip if not #6 marker
 	mov db6, dbx		; Move complete byte into register
-	rjmp loop			; Start over
+	rjmp loop		; Start over
 
 Set_One_Bit:	
 	ldi bvr, 1<<ONE		; Set ONE in bvr
@@ -328,39 +328,39 @@ Set_Zero_Bit:
 	ldi bvr, 1<<ZRO		; Set ZRO in bvr
 	rjmp Store_Bits		; Start over
 Clr_Bit_Val:	
-	clr bvr				; Clear all bits in bvr
-	rjmp loop			; Start over
+	clr bvr			; Clear all bits in bvr
+	rjmp loop		; Start over
 Set_Frame_Bit:	
 	ldi bvr, 1<<FRM		; Set FRM in bvr
-	clr bnr				; Clear bit number register
-	ldi mnr, 1			; Reset marker bit position at start of minute
-	rjmp loop			; Start over
+	clr bnr			; Clear bit number register
+	ldi mnr, 1		; Reset marker bit position at start of minute
+	rjmp loop		; Start over
 
 ; The following function takes the ONEs and ZROs and arranges them in a meaningful way into registers
 ; !!! THE FOLLOWING BRANCHES ARE OUT OF RANGE - CODE NEEDS TO BE ADJUSTED !!!
 Store_Bits:	
-	cpi bnr, 4			; Check if middle bit (unused)
+	cpi bnr, 4		; Check if middle bit (unused)
 	breq Do_Not_Store	; Skip if middle bit
-	cpi bnr, 14			; Check if middle bit (unused)
+	cpi bnr, 14		; Check if middle bit (unused)
 	breq Do_Not_Store	; Skip if middle bit
-	cpi bnr, 24			; Check if middle bit (unused)
+	cpi bnr, 24		; Check if middle bit (unused)
 	breq Do_Not_Store	; Skip if middle bit
-	cpi bnr, 34			; Check if middle bit (unused)
+	cpi bnr, 34		; Check if middle bit (unused)
 	breq Do_Not_Store	; Skip if middle bit
-	cpi bnr, 44			; Check if middle bit (unused)
+	cpi bnr, 44		; Check if middle bit (unused)
 	breq Do_Not_Store	; Skip if middle bit
-	cpi bnr, 54			; Check if middle bit (unused)
+	cpi bnr, 54		; Check if middle bit (unused)
 	breq Do_Not_Store	; Skip if middle bit
-	lsl dbx				; Prepare for incoming bit by shifting reg left
+	lsl dbx			; Prepare for incoming bit by shifting reg left
 	sbrc bvr, ONE		; Check for a ONE bit and if there is a ONE bit:
 	sbr dbx, ONE		; Set a 1 in the 0th bit location
 Do_Not_Store:
-	rjmp loop			; Start over
+	rjmp loop		; Start over
 
 ; Because bits are serially shifted through both displays, all 32 bits need to be sent sequentially
 Display_Write:
 	cbi PORTB, PORTB2	; Pull down /SS/PB2 pin to start data transfer
-	nop					; Give PORTB a cycle to adjust before allowing data xfer
+	nop			; Give PORTB a cycle to adjust before allowing data xfer
 	sts SPDR0, d1h		; Send display 1 (right display) address bits
 	rcall SPI_DR_Check	; Check serial transmission status before proceeding
 	sts SPDR0, d1l		; Send display 1 (right display) data bits
